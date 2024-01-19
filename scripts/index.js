ee7a3fccca84e588be86869208effa02d8bed6e9
Elena Ershova <jelena.ersh@yandex.ru>
2024-01-19 16:23:52 +0300

    fixed js

--- scripts/index.js
+++ scripts/index.js
@@ -8,33 +8,28 @@ const placesItem = cardTemplate.querySelector('.places__item');
 
 const placesList = document.querySelector('.places__list');
 
-// @todo: Функция удаления карточки
-
-let removeCard = function (event) {
-  let cardDelete = placesList.querySelector(`#${event.target.id}`);
-  cardDelete.parentElement.remove();
-};
-
 // @todo: Функция создания карточки
 
-const createCard = function(cardName, cardLink, num) {
+const createCard = function(cardName, cardLink, removeCard) {
 
   let placeItem = placesItem.cloneNode(true);
 
   placeItem.querySelector('.card__image').src = cardLink;
   placeItem.querySelector('.card__image').alt = `Фотография места в России - ${cardName}`;
   placeItem.querySelector('.card__title').textContent = cardName;
-  
-  let buttonRemove = placeItem.querySelector('.card__delete-button');
-
-  buttonRemove.id = `card_${num + 1}`;
-  buttonRemove.addEventListener('click', removeCard);
+  placeItem.querySelector('.card__delete-button').addEventListener('click', removeCard, true);
 
   return placeItem;
 }
 
+// @todo: Функция удаления карточки
+
+const removeCard = function(event) {
+  event.target.closest('.card').remove();
+};
+
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach((elem, num) => {
-  placesList.append(createCard(elem.name, elem.link, num));
+initialCards.forEach((elem) => {
+  placesList.append(createCard(elem.name, elem.link, removeCard));
 });
